9630b08795cb53520a734c8fa32a86f8ad93ec1b
Sergey <domarch388640@gmail.com>
2020-03-06 21:39:24 +0200

    commit del_files

---
+++ src/Link.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Link = React.createElement(
+  'a',
+  {
+    href: 'https://reactjs.org/',
+    target: '_blank',
+    rel: 'noreferrer noopener',
+  },
+  'Ссылка на reactjs.org',
+);
+
+console.log(Link);
+
+
+export default Link;
